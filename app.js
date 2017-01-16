57fbcfb39067f384574ff453a82028329728f43c
ms_vague <haha.face@gmail.com>
2017-01-16 16:20:50 -0600

    adding index.html, main.css, app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+var questions = [
+  {
+	question: 'Where is Orphan Black filmed?',
+	answer: ['Toronto, Canada', 'New Orleans, LA', 'New York, New York', 'Athens, Greece'],
+	response: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)",
+    wrongAnswer: 'Bzz! What would Helena think?'
+  },
+  
+  {
+	question: 'What is the name of the cloning program?',
+    answer: ['Project Project', 'Project X', 'Project Leda', 'Project so Many of Us'],
+	response: 'Ding-ding! Project Leda is correct!',
+	wrong:	"It's okay, there's always tomorrow."
+  },
+
+  {
+	question: 'What does Rachel love?',
+	answer: ['Power', 'Her Mom and Dad', 'Sarah', 'Windows'],
+	response: 'Windows',
+	wrong: "Oh wow, do you belong to The Clone Club?"
+  },
+
+  {
+	question: 'What does Cosima love?',
+	answer: ['Science', 'Her sisters', 'French Blonde Scientists', 'Pot', 'All of the Above'],
+	response: "Right on!",
+	wrong: "Err, at least you tried?"
+
+  },
+  {
+	question: 'We make a family, yes?',
+	answer: ['Put a ring on it', "I've already got a family", 'Er, no?', 'Aah!'],
+	response: 'Righto! This, I like.',
+	wrong: "¯\_(ツ)_/¯"
+	},
+];
+
