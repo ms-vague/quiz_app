e8685260bdb2262f1892c9ca73b836ce698eda79
ms_vague <haha.face@gmail.com>
2017-01-18 14:40:06 -0600

    restored JS questions, fixed HTML button form

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
 var questions = [
-  {
+  {	
 	question: 'Where is Orphan Black filmed?',
-	answer: ['Toronto, Canada', 'New Orleans, LA', 'New York, New York', 'Athens, Greece'],
-	response: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)",
+	options: ['Toronto, Canada', 'New Orleans, LA', 'New York, New York', 'Athens, Greece'],
+	answer: 'Toronto, Canada',
+	correct: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)",
     wrongAnswer: 'Bzz! What would Helena think?'
   },
   
-  {
+  { 
 	question: 'What is the name of the cloning program?',
-    answer: ['Project Project', 'Project X', 'Project Leda', 'Project so Many of Us'],
-	response: 'Ding-ding! Project Leda is correct!',
+    options: ['Project Project', 'Project X', 'Project Leda', 'Project so Many of Us'],
+    answer: 'Project Leda',
+	correct: "Great Scott, I've created life itself.",
 	wrong:	"It's okay, there's always tomorrow."
   },
 
-  {
+  {	
 	question: 'What does Rachel love?',
-	answer: ['Power', 'Her Mom and Dad', 'Sarah', 'Windows'],
-	response: 'Windows',
-	wrong: "Oh wow, do you belong to The Clone Club?"
+	options: ['Power', 'Her Mom and Dad', 'Sarah', 'Windows'],
+	answer: 'Windows',
+	correct: "Oh wow, do you belong to The Clone Club?",
+	wrong: "I'm so bored."
   },
 
   {
 	question: 'What does Cosima love?',
-	answer: ['Science', 'Her sisters', 'French Blonde Scientists', 'Pot', 'All of the Above'],
-	response: "Right on!",
-	wrong: "Err, at least you tried?"
+	options: ['Science', 'Her sisters', 'French Blonde Scientists', 'Pot', 'All of the Above'],
+	answer: 'All of the Above.'
+	correct: "Right on!",
+	wrong: 'Err, at least you tried?'
 
   },
   {
 	question: 'We make a family, yes?',
-	answer: ['Put a ring on it', "I've already got a family", 'Er, no?', 'Aah!'],
-	response: 'Righto! This, I like.',
-	wrong: "¯\_(ツ)_/¯"
+	options: ['Put a ring on it', "I've already got a family", 'Er, no?', 'Aah!'],
+	answer: "I've already got a family.",
+	correct: 'This, I like.',
+	wrong: 'You make me cry, sestra.'
 	},
+  {
+  	question: "You know what's a very French thing to do?",
+	options: ['Sing La Vie En Rose', 'Smoke a nice little cigarette', 'Watch Amelie', 'Monitor clones'],
+	answer: 'Smoke a nice little cigarette',
+	correct: 'Oui, brava!',
+	wrong: "No. I have to go. It-it's okay. Bye."
+  	},
+  {
+  	question: "What's Sarah's term of endearment for her daughter Kira?",
+	options: ['Lizard', 'Mini Clone', 'Loonie', 'Monkey'],
+	answer: 'Loonie',
+	correct: 'Nice job, eh?',
+	wrong: "I'm the smallest kid in my class."
+  	},
+
 ];
 
