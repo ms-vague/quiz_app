51c88e58dbc9ebc75dd5909c0f0e36188f82d594
ms_vague <haha.face@gmail.com>
2017-01-22 16:50:16 -0600

    not working but working on the JS

--- app.js
+++ app.js
@@ -1,12 +1,98 @@
+var questions = [
+  {	
+	question: 'Where is Orphan Black filmed?',
+	options: ['Toronto, Canada', 'New Orleans, LA', 'New York, New York', 'Athens, Greece'],
+	choice: 0,
+	correct: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)",
+  },
+  { 
+	question: 'What is the name of the cloning program?',
+    options: ['Project Project', 'Project X', 'Project Leda', 'Project so Many of Us'],
+    choice: 2,
+	correct: "Great Scott, I've created life itself.",
+  },
+  {	
+	question: 'What does Rachel love?',
+	options: ['Power', 'Her Mom and Dad', 'Sarah', 'Windows'],
+	choice: 3,
+	correct: "Oh wow, do you belong to The Clone Club?",
+  },
+    {	
+	question: "What's the name of Sarah's step-brother?",
+	options: ['Roger', 'Johnny', 'Felix', 'Mickey'],
+	choice: 2,
+	correct: "Holy shite, that worked!",
+  },
+  {
+	question: 'What does Cosima love?',
+	options: ['Her sisters', 'French Blonde Scientists', 'Pot', 'All of the Above'],
+	choice: 3,
+	correct: "Right on!",
+  },
+  {
+	question: 'We make a family, yes?',
+	options: ['Put a ring on it', "I've already got a family", 'Er, no?', 'Aah!'],
+	choice: 1,
+	correct: 'This, I like.',
+	},
+  {
+  	question: "You know what's a very French thing to do?",
+	options: ['Sing La Vie En Rose', 'Smoke a nice little cigarette', 'Watch Amelie', 'Monitor clones'],
+	choice: 1,
+	correct: 'Oui, brava!',
+  	},
+  {
+  	question: "What's Sarah's term of endearment for her daughter Kira?",
+	options: ['Lizard', 'Mini Clone', 'Loonie', 'Monkey'],
+	choice: 3,
+	correct: 'Nice job, eh?',
+  	},
+  {
+  	question: "Sarah, Cal, and Kira are collectively referred to as?",
+	options: ['The Fugitive Family', 'Hacker Fam', 'Oi! Fam', 'Lumberfamily'],
+	choice: 3,
+	correct: 'Nice job, meathead.',
+  	},
+  {
+  	question: "Sarah, who is Beth Childs?",
+	options: ["Who isn't Beth Childs, eh?", "Sarah's entry point into the clone conspiracy", "Paul's monitor", "Not a clone, that's for sure"],
+	choice: 1,
+	correct: "You're damn right.",
+  	}
+];
+
+var currentQuestion = 0;
+var totalQuestions = questions.length;
+var totalCorrectAnswers = 0;
+
+function startGame(){
+	$('.question').text(questions[currentQuestionNumber].question);
+	$('.answers').empty();
+
+	var numberOfChoices = questions[currentQuestionNumber].options.length;
+
+	for(var i = 0; i < numberOfChoices; i++){
+		var buildChoiceHTML = '<input class='answer' type='radio' name='option' value="' + i + '">' +
+		questions[currentQuestionNumber].questionChoices[i] + "<br/>";
+		$('.answers').append(buildChoiceHTML);
+	}
+
+	$('.count').text('Question ' + (currentQuestion + 1) + 'of' + totalQuestions);
+	$('.score').text('You have' + (totalCorrectAnswers) + 'of' + totalQuestions + 'correct.');
+}
+
 $(document).ready(function(){
 	$('.quiz_name').show();
 	$('.begin').show();
 	$('.button_start').show();
 	$('.question').hide();
 	$('.answers').hide();
-	$('.right_wrong').hide();
+	$('.count').hide();
+	$('.score').hide();
 	$('.button_again').hide();
 	$('.image_container').hide();
+
+	// start quiz //
 	$('.button_start').on('click', function(event){
 		event.preventDefault();
 	$('.question').show();
@@ -14,93 +100,61 @@ $(document).ready(function(){
 	$('.quiz_name').hide(); 
 	$('.introduction').hide(); 
 	$('.begin').hide(); 
-	$('.right_wrong').hide(); 
+	$('.count').hide();
+	$('.score').hide(); 
 	$('.button_again').hide(); 
 	$('.image_container').show();
 	$('.button_start').hide();
 	});
 });
 
-/* 
-*	implement some JS here for the questions and answers 
-*	followed by jQuery event stuff?
-*/
 
 
 
-var questions = [
-  {	
-	question: 'Where is Orphan Black filmed?',
-	options: ['Toronto, Canada', 'New Orleans, LA', 'New York, New York', 'Athens, Greece'],
-	answer: 'Toronto, Canada',
-	correct: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)",
-    wrong: 'Bzz! What would Helena think?'
-  },
-  { 
-	question: 'What is the name of the cloning program?',
-    options: ['Project Project', 'Project X', 'Project Leda', 'Project so Many of Us'],
-    answer: 'Project Leda',
-	correct: "Great Scott, I've created life itself.",
-	wrong:	"It's okay, there's always tomorrow."
-  },
-  {	
-	question: 'What does Rachel love?',
-	options: ['Power', 'Her Mom and Dad', 'Sarah', 'Windows'],
-	answer: 'Windows',
-	correct: "Oh wow, do you belong to The Clone Club?",
-	wrong: "I'm so bored."
-  },
-    {	
-	question: "What's the name of Sarah's step-brother?",
-	options: ['Roger', 'Johnny', 'Felix', 'Mickey'],
-	answer: 'Felix',
-	correct: "Holy shite, that worked!",
-	wrong: "Woops."
-  },
-  {
-	question: 'What does Cosima love?',
-	options: ['Science', 'Her sisters', 'French Blonde Scientists', 'Pot', 'All of the Above'],
-	answer: 'All of the Above.',
-	correct: "Right on!",
-	wrong: 'Err, at least you tried?'
 
-  },
-  {
-	question: 'We make a family, yes?',
-	options: ['Put a ring on it', "I've already got a family", 'Er, no?', 'Aah!'],
-	answer: "I've already got a family.",
-	correct: 'This, I like.',
-	wrong: 'You make me cry, sestra.'
-	},
-  {
-  	question: "You know what's a very French thing to do?",
-	options: ['Sing La Vie En Rose', 'Smoke a nice little cigarette', 'Watch Amelie', 'Monitor clones'],
-	answer: 'Smoke a nice little cigarette',
-	correct: 'Oui, brava!',
-	wrong: "No. I have to go. It-it's okay. Bye."
-  	},
-  {
-  	question: "What's Sarah's term of endearment for her daughter Kira?",
-	options: ['Lizard', 'Mini Clone', 'Loonie', 'Monkey'],
-	answer: 'Loonie',
-	correct: 'Nice job, eh?',
-	wrong: "I'm the smallest kid in my class."
-  	},
-  {
-  	question: "Sarah, Cal, and Kira are collectively referred to as?",
-	options: ['The Fugitive Family', 'Hacker Fam', 'La Famila', 'Oi! Fam', 'Lumberfamily'],
-	answer: 'Lumberfamily',
-	correct: 'Nice job, meathead.',
-	wrong: "Right, so you don't know, either?"
-  	},
-  {
-  	question: "Sarah, who is Beth Childs?",
-	options: ["Who isn't Beth Childs, eh?", "Sarah's entry point into the clone conspiracy", "Paul's monitor", "Not a clone, that's for sure"],
-	answer: "Sarah's entry point into the clone conspiracy",
-	correct: "You're damn right.",
-	wrong: "Nope."
-  	}
-];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
