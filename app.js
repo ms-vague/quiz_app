60849ca8c9773d927eafe16d9953f869dd6a60f0
ms_vague <haha.face@gmail.com>
2017-01-22 22:22:06 -0600

    answer options still wont show up

--- app.js
+++ app.js
@@ -3,61 +3,61 @@ var questions = [
 	question: 'Where is Orphan Black filmed?',
 	options: ['Toronto, Canada', 'New Orleans, LA', 'New York, New York', 'Athens, Greece'],
 	choice: 0,
-	correct: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)"
+	correctMessage: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)"
   },
   { 
 	question: 'What is the name of the cloning program?',
     options: ['Project Project', 'Project X', 'Project Leda', 'Project so Many of Us'],
     choice: 2,
-	correct: "Great Scott, I've created life itself."
+	correctMessage: "Great Scott, I've created life itself."
   },
   {	
 	question: 'What does Rachel love?',
 	options: ['Power', 'Her Mom and Dad', 'Sarah', 'Windows'],
 	choice: 3,
-	correct: "Oh wow, do you belong to The Clone Club?"
+	correctMessage: "Oh wow, do you belong to The Clone Club?"
   },
     {	
 	question: "What's the name of Sarah's step-brother?",
 	options: ['Roger', 'Johnny', 'Felix', 'Mickey'],
 	choice: 2,
-	correct: "Holy shite, that worked!"
+	correctMessage: "Holy shite, that worked!"
   },
   {
 	question: 'What does Cosima love?',
 	options: ['Her sisters', 'French Blonde Scientists', 'Pot', 'All of the Above'],
 	choice: 3,
-	correct: "Right on!"
+	correctMessage: "Right on!"
   },
   {
 	question: 'We make a family, yes?',
 	options: ['Put a ring on it', "I've already got a family", 'Er, no?', 'Aah!'],
 	choice: 1,
-	correct: 'This, I like.'
+	correctMessage: 'This, I like.'
 	},
   {
   	question: "You know what's a very French thing to do?",
 	options: ['Sing La Vie En Rose', 'Smoke a nice little cigarette', 'Watch Amelie', 'Monitor clones'],
 	choice: 1,
-	correct: 'Oui, brava!'
+	correctMessage: 'Oui, brava!'
   	},
   {
   	question: "What's Sarah's term of endearment for her daughter Kira?",
 	options: ['Lizard', 'Mini Clone', 'Loonie', 'Monkey'],
 	choice: 3,
-	correct: 'Nice job, eh?'
+	correctMessage: 'Nice job, eh?'
   	},
   {
   	question: "Sarah, Cal, and Kira are collectively referred to as?",
 	options: ['The Fugitive Family', 'Hacker Fam', 'Oi! Fam', 'Lumberfamily'],
 	choice: 3,
-	correct: 'Nice job, meathead.'
+	correctMessage: 'Nice job, meathead.'
   	},
   {
   	question: "Sarah, who is Beth Childs?",
 	options: ["Who isn't Beth Childs, eh?", "Sarah's entry point into the clone conspiracy", "Paul's monitor", "Not a clone, that's for sure"],
 	choice: 1,
-	correct: "You're damn right."
+	correctMessage: "You're damn right."
   	}
 ];
 
@@ -74,7 +74,7 @@ function displayQuestion(){
 var totalChoices = questions[currentQuestion].options.length;
 
 for (var i = 0; i < totalChoices.length; i++) {
-	var buildOptionsHTML = '<input class="answer" type="radio" name="option" value="' + i + '">' + questions[currentQuestion].options[i] + '<br/>';
+	var buildOptionsHTML = '<input class="answer" type="radio"  name="option" value="' + i + '">' + questions[currentQuestion].options[i] + ' <br/> ';
 	$('.choices').append(buildOptionsHTML);
 }
 
@@ -86,22 +86,20 @@ $('.score').text('You have ' + (numberOfRightAnswers) + ' of ' + numberOfQuestio
 $(document).ready(function(){
 	$('.begin').show();
 	$('.quiz').hide();
-	$('.question').hide();
 	$('.results').hide();
 
 	// start quiz //
-	$('.button_start').on('click', function(event){
-		event.preventDefault();
-	$('.begin').hide();
+	$('.button_start').on('click', function(){
 	$('.results').hide();
-	$('.question').show();
+	$('.begin').hide();
 	$('.quiz').show();
 	$('.results').empty();
 	displayQuestion();
-	});
+});
 
 	// show questions
-	$('.question').on('click', '.answer', function(){
+	$('.quiz').on('click', '.answer', function(){
+
 		var userAnswer = $('input[class="answer"]:checked').val();
 
 		var rightAnswer = questions[currentQuestion].choice;
@@ -114,9 +112,9 @@ $(document).ready(function(){
 
 		if((currentQuestion + 1) === numberOfQuestions) {
 			$('.results').show();
+			$('.begin').hide();
 			$('.quiz').hide();
-
-			$('.results').text('You got' + (numberOfRightAnswers) + ' of ' + numberOfQuestions + ' correct.');
+			
 		}
 		else {
 			currentQuestion++;
@@ -126,16 +124,13 @@ $(document).ready(function(){
 
 	$('.results').on('click', '.again', function(){
 		$('.begin').show();
-		$('.button_start').show();
 		$('.quiz').hide();
 		$('.results').hide();
 
 		currentQuestion = 0;
 		numberOfRightAnswers = 0;
 	})
-
 });
-
 
 
 
