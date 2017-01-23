91b4020a8ef7d0f490fd9b23408820d6f14aa88c
ms_vague <haha.face@gmail.com>
2017-01-22 18:47:48 -0600

    took out extra commas in questions array

--- app.js
+++ app.js
@@ -3,61 +3,61 @@ var questions = [
 	question: 'Where is Orphan Black filmed?',
 	options: ['Toronto, Canada', 'New Orleans, LA', 'New York, New York', 'Athens, Greece'],
 	choice: 0,
-	correct: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)",
+	correct: "Correctomundo! Orphan Black is filmed in Toronto, Canada. (Hi, Drake)"
   },
   { 
 	question: 'What is the name of the cloning program?',
     options: ['Project Project', 'Project X', 'Project Leda', 'Project so Many of Us'],
     choice: 2,
-	correct: "Great Scott, I've created life itself.",
+	correct: "Great Scott, I've created life itself."
   },
   {	
 	question: 'What does Rachel love?',
 	options: ['Power', 'Her Mom and Dad', 'Sarah', 'Windows'],
 	choice: 3,
-	correct: "Oh wow, do you belong to The Clone Club?",
+	correct: "Oh wow, do you belong to The Clone Club?"
   },
     {	
 	question: "What's the name of Sarah's step-brother?",
 	options: ['Roger', 'Johnny', 'Felix', 'Mickey'],
 	choice: 2,
-	correct: "Holy shite, that worked!",
+	correct: "Holy shite, that worked!"
   },
   {
 	question: 'What does Cosima love?',
 	options: ['Her sisters', 'French Blonde Scientists', 'Pot', 'All of the Above'],
 	choice: 3,
-	correct: "Right on!",
+	correct: "Right on!"
   },
   {
 	question: 'We make a family, yes?',
 	options: ['Put a ring on it', "I've already got a family", 'Er, no?', 'Aah!'],
 	choice: 1,
-	correct: 'This, I like.',
+	correct: 'This, I like.'
 	},
   {
   	question: "You know what's a very French thing to do?",
 	options: ['Sing La Vie En Rose', 'Smoke a nice little cigarette', 'Watch Amelie', 'Monitor clones'],
 	choice: 1,
-	correct: 'Oui, brava!',
+	correct: 'Oui, brava!'
   	},
   {
   	question: "What's Sarah's term of endearment for her daughter Kira?",
 	options: ['Lizard', 'Mini Clone', 'Loonie', 'Monkey'],
 	choice: 3,
-	correct: 'Nice job, eh?',
+	correct: 'Nice job, eh?'
   	},
   {
   	question: "Sarah, Cal, and Kira are collectively referred to as?",
 	options: ['The Fugitive Family', 'Hacker Fam', 'Oi! Fam', 'Lumberfamily'],
 	choice: 3,
-	correct: 'Nice job, meathead.',
+	correct: 'Nice job, meathead.'
   	},
   {
   	question: "Sarah, who is Beth Childs?",
 	options: ["Who isn't Beth Childs, eh?", "Sarah's entry point into the clone conspiracy", "Paul's monitor", "Not a clone, that's for sure"],
 	choice: 1,
-	correct: "You're damn right.",
+	correct: "You're damn right."
   	}
 ];
 
